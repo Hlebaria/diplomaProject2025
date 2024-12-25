4e2f56c34f626ffe2fead4b2c4d222db88b32185
Hlebaria <mitigr@abv.bg>
2024-12-25 23:33:39 +0200

    Commnet out Dotenv to allow cloud env variable injection

--- backend2/MyChoiceApp/MyChoiceApp/src/main/java/com/choiceApp/MyChoiceApp/MyChoiceAppApplication.java
+++ backend2/MyChoiceApp/MyChoiceApp/src/main/java/com/choiceApp/MyChoiceApp/MyChoiceAppApplication.java
@@ -2,7 +2,7 @@ package com.choiceApp.MyChoiceApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class MyChoiceAppApplication {
--- backend2/MyChoiceApp/MyChoiceApp/src/test/java/com/choiceApp/MyChoiceApp/ConnectionTests.java
+++ backend2/MyChoiceApp/MyChoiceApp/src/test/java/com/choiceApp/MyChoiceApp/ConnectionTests.java
@@ -1,12 +1,12 @@
 package com.choiceApp.MyChoiceApp;
 
-import io.github.cdimascio.dotenv.Dotenv;
+//import io.github.cdimascio.dotenv.Dotenv;
 import org.junit.jupiter.api.Test;
-import org.postgresql.ds.PGConnectionPoolDataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+//import org.postgresql.ds.PGConnectionPoolDataSource;
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class ConnectionTests {
