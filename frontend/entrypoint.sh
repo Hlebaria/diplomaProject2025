e9d8cf10716ad8d2985ef2e711b99c4f812fe7d7
Hlebaria <mitigr@abv.bg>
2024-12-31 00:49:35 +0200

    env fix 8

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-envsubst '$REACT_APP_BACKEND_URL' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf
-
+envsubst '$REACT_APP_BACKEND_URL' < /etc/nginx/nginx.conf > /tmp/nginx.conf
+mv /tmp/nginx.conf /etc/nginx/nginx.conf
 nginx -g 'daemon off;'
