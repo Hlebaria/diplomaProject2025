0a48a7dbc19554e78aff4bcd12bfcea12048b560
Hlebaria <mitigr@abv.bg>
2024-12-31 04:25:20 +0200

    env fix 13

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,7 +2,7 @@
 
 echo "Backend URL: $REACT_APP_BACKEND_URL"
 
-envsubst '${REACT_APP_BACKEND_URL}' < /app/build/index.html > /tmp/index.html
-mv /tmp/index.html /app/build/index.html
+envsubst '${REACT_APP_BACKEND_URL}' < /usr/share/nginx/html/index.html > /tmp/index.html
+mv /tmp/index.html /usr/share/nginx/html/index.html
 
-serve -s /app/build -l 3000
+nginx -g 'daemon off;'
