7b990fcb23f691d3976b098f57dd77d74b9f5b4d
Hlebaria <mitigr@abv.bg>
2025-02-01 00:36:53 +0200

    adapt to keycloak

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,7 +2,8 @@
 
 echo "Backend URL: $REACT_APP_BACKEND_URL"
 
-envsubst '${REACT_APP_BACKEND_URL}' < /usr/share/nginx/html/index.html > /tmp/index.html
-mv /tmp/index.html /usr/share/nginx/html/index.html
+envsubst '${REACT_APP_BACKEND_URL} ${REACT_APP_KEYCLOAK_URL} ${REACT_APP_KEYCLOAK_REALM} ${REACT_APP_KEYCLOAK_CLIENT_ID}' < /usr/share/nginx/html/variableInject.js > /tmp/variableInject.js
+
+mv /tmp/variableInject.js /usr/share/nginx/html/variableInject.js
 
 nginx -g 'daemon off;'
---
+++ frontend/public/variableInject.js
@@ -0,0 +1,29 @@
+// window.REACT_APP_BACKEND_URL = "${REACT_APP_BACKEND_URL}";
+// if (window.REACT_APP_BACKEND_URL = "${REACT_APP_" + "BACKEND_URL}"){
+// window.REACT_APP_BACKEND_URL = undefined;
+// }
+// window.REACT_APP_KEYCLOAK_URL = "${REACT_APP_KEYCLOAK_URL}";
+// if (window.REACT_APP_KEYCLOAK_URL = "${REACT_APP_" + "KEYCLOAK_URL}"){
+// window.REACT_APP_KEYCLOAK_URL = undefined;
+// }
+// window.REACT_APP_KEYCLOAK_REALM = "${REACT_APP_KEYCLOAK_REALM}";
+// if (window.REACT_APP_KEYCLOAK_REALM = "${REACT_APP_" + "KEYCLOAK_REALM}"){
+// window.REACT_APP_KEYCLOAK_REALM = undefined;
+// }
+// window.REACT_APP_KEYCLOAK_CLIENT_ID = "${REACT_APP_KEYCLOAK_CLIENT_ID}";
+// if (window.REACT_APP_KEYCLOAK_CLIENT_ID = "${REACT_APP_" + "KEYCLOAK_CLIENT_ID}"){
+// window.REACT_APP_KEYCLOAK_CLIENT_ID = undefined;
+// }
+
+function resolveEnvVariable(envVar) {
+    return envVar.includes("${REACT_APP_") ? undefined : envVar;
+}
+
+window.REACT_APP_BACKEND_URL = resolveEnvVariable("${REACT_APP_BACKEND_URL}");
+window.REACT_APP_KEYCLOAK_URL = resolveEnvVariable("${REACT_APP_KEYCLOAK_URL}");
+window.REACT_APP_KEYCLOAK_REALM = resolveEnvVariable("${REACT_APP_KEYCLOAK_REALM}");
+window.REACT_APP_KEYCLOAK_CLIENT_ID = resolveEnvVariable("${REACT_APP_KEYCLOAK_CLIENT_ID}");
+
+
+console.log("Backend URL is: " + window.REACT_APP_BACKEND_URL);
+console.log("keycloak things: " + window.REACT_APP_KEYCLOAK_URL + window.REACT_APP_KEYCLOAK_REALM + window.REACT_APP_KEYCLOAK_CLIENT_ID);
