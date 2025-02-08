c98ca7e2a7fd2b60863e0e28a73e2ced282135f3
Hlebaria <mitigr@abv.bg>
2025-02-08 20:56:15 +0200

    push down reference code

--- frontend/public/variableInject.js
+++ frontend/public/variableInject.js
@@ -1,3 +1,17 @@
+
+function resolveEnvVariable(envVar) {
+    return envVar.includes("${REACT_APP_") ? undefined : envVar;
+}
+
+window.REACT_APP_BACKEND_URL = resolveEnvVariable("${REACT_APP_BACKEND_URL}");
+window.REACT_APP_KEYCLOAK_URL = resolveEnvVariable("${REACT_APP_KEYCLOAK_URL}");
+window.REACT_APP_KEYCLOAK_REALM = resolveEnvVariable("${REACT_APP_KEYCLOAK_REALM}");
+window.REACT_APP_KEYCLOAK_CLIENT_ID = resolveEnvVariable("${REACT_APP_KEYCLOAK_CLIENT_ID}");
+
+
+console.log("Backend URL is: " + window.REACT_APP_BACKEND_URL);
+console.log("keycloak things: " + window.REACT_APP_KEYCLOAK_URL + window.REACT_APP_KEYCLOAK_REALM + window.REACT_APP_KEYCLOAK_CLIENT_ID);
+
 // window.REACT_APP_BACKEND_URL = "${REACT_APP_BACKEND_URL}";
 // if (window.REACT_APP_BACKEND_URL = "${REACT_APP_" + "BACKEND_URL}"){
 // window.REACT_APP_BACKEND_URL = undefined;
@@ -14,16 +28,3 @@
 // if (window.REACT_APP_KEYCLOAK_CLIENT_ID = "${REACT_APP_" + "KEYCLOAK_CLIENT_ID}"){
 // window.REACT_APP_KEYCLOAK_CLIENT_ID = undefined;
 // }
-
-function resolveEnvVariable(envVar) {
-    return envVar.includes("${REACT_APP_") ? undefined : envVar;
-}
-
-window.REACT_APP_BACKEND_URL = resolveEnvVariable("${REACT_APP_BACKEND_URL}");
-window.REACT_APP_KEYCLOAK_URL = resolveEnvVariable("${REACT_APP_KEYCLOAK_URL}");
-window.REACT_APP_KEYCLOAK_REALM = resolveEnvVariable("${REACT_APP_KEYCLOAK_REALM}");
-window.REACT_APP_KEYCLOAK_CLIENT_ID = resolveEnvVariable("${REACT_APP_KEYCLOAK_CLIENT_ID}");
-
-
-console.log("Backend URL is: " + window.REACT_APP_BACKEND_URL);
-console.log("keycloak things: " + window.REACT_APP_KEYCLOAK_URL + window.REACT_APP_KEYCLOAK_REALM + window.REACT_APP_KEYCLOAK_CLIENT_ID);
